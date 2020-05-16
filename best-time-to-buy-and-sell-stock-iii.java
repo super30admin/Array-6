1c9db5619b325470aec1d97bba14aabe88a77eb5
Suraj Raghavendra Vadvadgi <45305881+surajrv35@users.noreply.github.com>
2020-05-16 00:40:22 -0500

    Update best-time-to-buy-and-sell-stock-iii.java

--- best-time-to-buy-and-sell-stock-iii.java
+++ best-time-to-buy-and-sell-stock-iii.java
@@ -3,11 +3,14 @@
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyingPrice=Integer.MAX_VALUE, profit=0;
+        int buyingPrice1=Integer.MAX_VALUE, buyingPrice2=Integer.MAX_VALUE, profit1=0, profit2=0;
         for(int price : prices) {
-            buyingPrice=Math.min(buyingPrice, price);
-            profit=Math.max(profit, price-buyingPrice);
+            buyingPrice1=Math.min(buyingPrice1, price);
+            profit1=Math.max(profit1, price-buyingPrice1);
+            
+            buyingPrice2=Math.min(buyingPrice2, price-profit1);
+            profit2=Math.max(profit2, price-buyingPrice2);
         }
-        return profit;
+        return profit2;
     }
 }
