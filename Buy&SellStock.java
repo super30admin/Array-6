bac9e75399a1048b6b68da5feac1d5e4f7bd0ce9
Jeel Patel <35517028+jeelpatel18@users.noreply.github.com>
2022-06-22 11:50:12 -0500

    Update Buy&SellStock.java

--- Buy&SellStock.java
+++ Buy&SellStock.java
@@ -7,11 +7,11 @@ class Solution {
         int min = prices[0];
         
        for(int i = 1; i< prices.length; i++){
-           min = Math.min(min, prices[i]);
-           max = Math.max(max, prices[i] - min);
+           min = Math.min(min, prices[i]); // Everytime checking min to buy
+           max = Math.max(max, prices[i] - min); // checking the maxProfit we can get from buyb price
        }
         
-        return max;
+        return max; // returning maxProfit we can get
     }
 }
 
