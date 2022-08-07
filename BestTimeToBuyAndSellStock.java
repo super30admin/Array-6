b1756ee86a6d9a322a1cfc4dee46df2fbaaa5219
Arun Rajachandar R <arunrajachandarr@Aruns-Air.bbrouter>
2022-08-07 11:05:48 +0530

    [Arun] Array-6 completed

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,13 @@
+//time: O(N)| Space: O(1)
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int max = 0;
+        for(int i=1;i<prices.length;i++) {
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i]-min);
+        }
+        return max;
+    }
+}
